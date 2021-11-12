0eea38de5e3f9b6bb70cfcc812ac4319be6ac5e9
jkv27100 <jagannathkvjkv@gmail.com>
2021-11-12 22:01:50 +0530

    nav bar and section markup and basic styling is done

---
+++ main.js
@@ -0,0 +1,16 @@
+// ScrollReveal().reveal(".sec1-txt1", {
+//   delay: 500,
+//   rotate: {
+//     x: 20,
+//     z: 20,
+//   },
+// });
+
+// ScrollReveal().reveal(".sec1-txt2", {
+//   delay: 700,
+//   easing: "cubic-bezier(0.5, 0, 0, 1)",
+//   rotate: {
+//     x: 20,
+//     z: 20,
+//   },
+// });
